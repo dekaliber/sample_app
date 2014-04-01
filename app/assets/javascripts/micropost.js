1bba6f42d3e5987829ee6868b0028a1559957594
tai@dekaliber.net <tai@dekaliber.net>
2014-04-01 14:30:36 -0700

    Added follow/unfollow

--- app/assets/javascripts/micropost.js
+++ app/assets/javascripts/micropost.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	document.getElementById('micropost_content').onkeyup=function() {
-    document.getElementById('charCount').innerHTML = 140-this.value.length;    
+	if (document.getElementById('micropost_content')) {
+		document.getElementById('micropost_content').onkeyup=function() {
+	    document.getElementById('charCount').innerHTML = 140-this.value.length;    
+		}
 	}
 });
